e832b8b8f2cf279f52af18fcf47bb0f99e4952f3
larrylam <220155672@stu.vtc.edu.hk>
2023-09-25 18:24:48 +0800

    larrylam

--- 1 - level/code/level.py
+++ 1 - level/code/level.py
@@ -1,4 +1,4 @@
-import pygame 
+import pygame
 from settings import *
 from tile import Tile
 from player import Player
@@ -6,52 +6,49 @@ from debug import debug
 
 
 class Level:
-	def __init__(self):
+    def __init__(self):
+        # get the display surface
+        self.display_surface = pygame.display.get_surface()
 
-		# get the display surface 
-		self.display_surface = pygame.display.get_surface()
+        # sprite group setup
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
 
-		# sprite group setup
-		self.visible_sprites = pygame.sprite.Group()
-		self.obstacle_sprites = pygame.sprite.Group()
+        # sprite setup
+        self.create_map()
 
-		# sprite setup
-		self.create_map()
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == "x":
+                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+                if col == "p":
+                    self.player = Player((x, y), [self.visible_sprites], [])
 
-	def create_map(self):
-		for row_index,row in enumerate(WORLD_MAP):
-			for col_index, col in enumerate(row):
-				x = col_index * TILESIZE
-				y = row_index * TILESIZE
-				if col == 'x':
-					Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
-				if col == 'p':
-				  self.player =	Player((x,y),[self.visible_sprites],self.obstacle_sprites)
+    def run(self):
+        self.display_surface.fill((0, 0, 0))
+        self.visible_sprites.update()
+        for sprite in self.visible_sprites:
+            self.display_surface.blit(sprite.image, sprite.rect)
+        pygame.display.flip()
 
-	def run(self):
-		# update and draw the game
-		self.visible_sprites.draw(self.player)
-		self.visible_sprites.update()
-	
 
 class YSortCameraGroup(pygame.sprite.Group):
-	def __init__(self):
+    def __init__(self):
+        # general setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vectors2()
 
-		# general setup
-		super().__init__()
-		self.display_surface = pygame.display.get_surface()
-		self.half_width = self.display_surface.get_size()[0] // 2
-		self.half_height = self.display_surface.get_size()[1] // 2
-		self.offset = pygame.math.Vectors()
+    def custom_draw(self, player):
+        # getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
 
-	def custom_draw(self,player):
-
-		#getting the offset
-		self.offset.x = player.rect.centerx - self.half_width
-		self.offset.y = player.rect.centery - self.half_height
-
-		for sprite in self.sprites():
-			offset_pos = sprite.rect.topleft - self.offset
-			self.display_surface.blit(sprite.image,offset_pos)
-
-
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
--- 1 - level/code/tile.py
+++ 1 - level/code/tile.py
@@ -1,9 +1,12 @@
-import pygame 
+import pygame
 from settings import *
 
+
 class Tile(pygame.sprite.Sprite):
-	def __init__(self,pos,groups):
-		super().__init__(groups)
-		self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
-		self.rect = self.image.get_rect(topleft = pos)
-		self.hitbox = self.rect.inflate(0,-10)
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(
+            "1 - level/graphics/test/rock.png"
+        ).convert_alpha()
+        self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0, -10)
