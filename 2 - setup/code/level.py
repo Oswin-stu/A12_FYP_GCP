9de76d8efa5234b60896a70178e84bd14c02b622
Oswin-stu <220072750@stu.vtc.edu.hk>
2023-11-13 17:58:37 +0800

    animation01

--- 2 - setup/code/level.py
+++ 2 - setup/code/level.py
@@ -56,6 +56,7 @@ class Level:
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        debug(self.player.status)
 
 class YSortCarmeraGroup(pygame.sprite.Group):
     def __init__(self):
