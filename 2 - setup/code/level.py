c54a79f0680f2d63b999d987fc09e1dc8e37ab18
Oswin-stu <220072750@stu.vtc.edu.hk>
2023-12-19 16:58:54 +0800

    Enemy creation

--- 2 - setup/code/level.py
+++ 2 - setup/code/level.py
@@ -1,6 +1,4 @@
-from typing import Iterable, Union
 import pygame
-from pygame.sprite import AbstractGroup
 from settings import *
 from tile import Tile
 from player import Player
@@ -9,10 +7,11 @@ from support import *
 from random import choice
 from weapon import Weapon
 from ui import UI
+from enemy import Enemy
+
 
 class Level:
     def __init__(self):
-
         # get the display surface
         self.display_surface = pygame.display.get_surface()
 
@@ -31,88 +30,131 @@ class Level:
 
     def create_map(self):
         layout = {
-            'boundary': import_csv_layout('/Users/oswin/Desktop/Github FYP/A12_FYP_GCP/1 - level/map/map_FloorBlocks.csv'),
-            'grass': import_csv_layout('/Users/oswin/Desktop/Github FYP/A12_FYP_GCP/1 - level/map/map_Grass.csv'),
-            'object': import_csv_layout('/Users/oswin/Desktop/Github FYP/A12_FYP_GCP/1 - level/map/map_LargeObjects.csv'),
+            "boundary": import_csv_layout(
+                "/Users/oswin/Desktop/Github FYP/A12_FYP_GCP/1 - level/map/map_FloorBlocks.csv"
+            ),
+            "grass": import_csv_layout(
+                "/Users/oswin/Desktop/Github FYP/A12_FYP_GCP/1 - level/map/map_Grass.csv"
+            ),
+            "object": import_csv_layout(
+                "/Users/oswin/Desktop/Github FYP/A12_FYP_GCP/1 - level/map/map_LargeObjects.csv"
+            ),
+            "entities": import_csv_layout(
+                "/Users/oswin/Desktop/Github FYP/A12_FYP_GCP/1 - level/map/map_Entities.csv"
+            ),
         }
         graphics = {
-            'grass': import_folder('/Users/oswin/Desktop/Github FYP/A12_FYP_GCP/1 - level/graphics/grass'),
-            'object': import_folder('/Users/oswin/Desktop/Github FYP/A12_FYP_GCP/1 - level/graphics/objects')
+            "grass": import_folder(
+                "/Users/oswin/Desktop/Github FYP/A12_FYP_GCP/1 - level/graphics/grass"
+            ),
+            "object": import_folder(
+                "/Users/oswin/Desktop/Github FYP/A12_FYP_GCP/1 - level/graphics/objects"
+            ),
         }
 
-        for style,layout in layout.items():
+        for style, layout in layout.items():
             for row_index, row in enumerate(layout):
                 for col_index, col in enumerate(row):
-                    if col != '-1':
+                    if col != "-1":
                         x = col_index * TILESIZE
                         y = row_index * TILESIZE
-                        if style == 'boundary':
-                            Tile((x,y),[self.obstacle_sprites],'invisible')
+                        if style == "boundary":
+                            Tile((x, y), [self.obstacle_sprites], "invisible")
 
-                        if style == 'grass':
-                            random_grass_image = choice(graphics['grass'])
-                            Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'grass',random_grass_image)
+                        if style == "grass":
+                            random_grass_image = choice(graphics["grass"])
+                            Tile(
+                                (x, y),
+                                [self.visible_sprites, self.obstacle_sprites],
+                                "grass",
+                                random_grass_image,
+                            )
 
-                        if style == 'object':
-                            surf = graphics['object'][int(col)]
-                            Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'object',surf)
+                        if style == "object":
+                            surf = graphics["object"][int(col)]
+                            Tile(
+                                (x, y),
+                                [self.visible_sprites, self.obstacle_sprites],
+                                "object",
+                                surf,
+                            )
 
-        self.player = Player(
-            (2000,1430),
-            [self.visible_sprites],
-            self.obstacle_sprites,
-            self.create_attack,
-            self.destory_attack,
-            self.create_magic)
-           
+                        if style == "entities":
+                            if col == "394":
+                                self.player = Player(
+                                    (x, y),
+                                    [self.visible_sprites],
+                                    self.obstacle_sprites,
+                                    self.create_attack,
+                                    self.destroy_attack,
+                                    self.create_magic
+                                )
+                            else:
+                                if col == "390":
+                                    monster_name = "bamboo"
+                                elif col == "391":
+                                    monster_name = "spirit"
+                                elif col == "392":
+                                    monster_name = "raccoon"
+                                else:
+                                    monster_name = "squid"
+                                Enemy(
+                                    monster_name,
+                                    (x, y),
+                                    [self.visible_sprites],self.obstacle_sprites
+                                )
+
     def create_attack(self):
-        
-        self.create_attack = Weapon(self.player,[self.visible_sprites])
-    
-    def create_magic(self,style,strength,cost):
+        self.create_attack = Weapon(self.player, [self.visible_sprites])
+
+    def create_magic(self, style, strength, cost):
         print(style)
         print(strength)
         print(cost)
 
-    def destory_attack(self):
+    def destroy_attack(self):
         if self.create_attack:
             self.create_attack.kill()
         self.create_attack = None
-
 
     def run(self):
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        self.visible_sprites.enemy_update(self.player)
         self.ui.display(self.player)
 
 
 class YSortCarmeraGroup(pygame.sprite.Group):
     def __init__(self):
-
         # general setup
         super().__init__()
         self.display_surface = pygame.display.get_surface()
         self.half_width = self.display_surface.get_size()[0] // 2
         self.half_height = self.display_surface.get_size()[1] // 2
-        self.offset = pygame.math.Vector2(100,200)
+        self.offset = pygame.math.Vector2(100, 200)
 
         # creating the floor
-        self.floor_surf = pygame.image.load('/Users/oswin/Desktop/Github FYP/A12_FYP_GCP/7 - Weapon/graphics/tilemap/ground.png').convert()
-        self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
+        self.floor_surf = pygame.image.load(
+            "/Users/oswin/Desktop/Github FYP/A12_FYP_GCP/7 - Weapon/graphics/tilemap/ground.png"
+        ).convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft=(0, 0))
 
-    def custom_draw(self,player):
-
+    def custom_draw(self, player):
         # getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
 
         # drawing the floor
         floor_offset_pos = self.floor_rect.topleft - self.offset
-        self.display_surface.blit(self.floor_surf,floor_offset_pos)
-
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
 
         # for sprite in self.sprites():
-        for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
+        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
-            self.display_surface.blit(sprite.image,offset_pos)
+            self.display_surface.blit(sprite.image, offset_pos)
+
+    def enemy_update(self,player):
+        enemy_sprites = [sprite for sprite in self.sprites() if hasattr(sprite,'sprite_type') and sprite.sprite_type == 'enemy']
+        for enemy in enemy_sprites:
+            enemy.enemy_update(player)
